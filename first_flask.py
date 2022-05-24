0958f559733f092b128bd660517429ce30ead3be
Oleksii Kovalov <kovalov.aleksey@gmail.com>
2022-05-24 18:21:15 +0100

    add contacts, home, registration pages

--- first_flask.py
+++ first_flask.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        import pprint
+        pprint.pprint(request.form['firstName'])
+        pprint.pprint(request.form['lastName'])
+        pprint.pprint(request.form['username'])
+        pprint.pprint(request.form['email'])
+        pprint.pprint(request.form['Phonenumber'])
+
+        pprint.pprint(request.form)
     return render_template('bla.html')
+
+
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/registration', methods=['GET'])
+def registration():
+    return render_template('registration.html')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return render_template('contacts.html')
 
 if __name__ == '__main__':
     app.run()
-
-
