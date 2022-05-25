3d2d754a59a679bec880ef655974f1126ed0063d
Oleksii Kovalov <kovalov.aleksey@gmail.com>
2022-05-25 18:45:12 +0100

    add connection from mysql

--- first_flask.py
+++ first_flask.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 from flask import request
+import mysql.connector
+import pprint
+
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+
+
     if request.method == 'POST':
-        import pprint
+
         pprint.pprint(request.form['firstName'])
         pprint.pprint(request.form['lastName'])
         pprint.pprint(request.form['username'])
@@ -15,8 +20,27 @@ def index():
         pprint.pprint(request.form)
 
         #mysql code here
+
+    if request.method == 'GET':
+        print("1")
+        conn = mysql.connector.connect(host="127.0.0.1", user="user_one", password="1q2w3e4r", database="Someweres", auth_plugin='mysql_native_password')
+        print("2")
+        cursor = conn.cursor()
+        print("3")
+        cursor.execute("""SELECT * FROM `clients` where ClientID = '4'""")
+        print("4")
+        clients = cursor.fetchall()
+        pprint.pprint(clients)
+
+
+        firstName=clients[0][1]
+        secondName = clients[0][2]
+        username = clients[0][3]
+        email = clients[0][4]
+        phone = clients[0][5]
+        sex = clients[0][6]
         
-    return render_template('bla.html')
+    return render_template('bla.html', firstName=firstName, secondName=secondName, username=username, email=email, phone=phone, sex=sex)
 
 
 
